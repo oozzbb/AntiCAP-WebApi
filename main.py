0a22d532875cd80d1d0634f237cf498cc7f21751
81NewArk <751247667@qq.com>
2025-05-30 12:10:55 +0800

    update

---
+++ main.py
@@ -0,0 +1,81 @@
+import AntiCAP
+import uvicorn
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+description = """
+* 通过Http协议 跨语言调用AntiCAP 
+
+* Author：NewArk81
+
+<img src="https://img.shields.io/badge/GitHub-ffffff"></a> <a href="https://github.com/81NewArk/AntiCAP"> <img src="https://img.shields.io/github/stars/81NewArk/AntiCAP?style=social"> <img src="https://badges.pufler.dev/visits/81NewArk/AntiCAP">
+
+"""
+
+
+
+app = FastAPI(title="AntiCAP - WebApi", description=description, version="1.0.1")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class ModelImageIn(BaseModel):
+    img_base64: str
+
+
+class ModelOrderImageIn(BaseModel):
+    order_img_base64: str
+    target_img_base64:str
+
+
+
+Atc = AntiCAP.AntiCAP(show_ad=False)
+
+
+
+
+@app.post("/ocr",summary="返回字符串",tags=["OCR识别"])
+async def ocr(data: ModelImageIn):
+    result = Atc.OCR(data.img_base64)
+    return {"result": result }
+
+@app.post("/math",summary="返回计算结果",tags=["算数识别"])
+async def math(data: ModelImageIn):
+    result = Atc.Math(data.img_base64)
+    return {"result": result }
+
+@app.post("/detection/icon",summary="检测图标,返回坐标",tags=["目标检测"])
+async def detection(data: ModelImageIn):
+    result = Atc.Detection_Icon(data.img_base64)
+    return {"result": result }
+
+
+@app.post("/detection/icon/order",summary="按序返回图标的坐标",tags=["目标检测"])
+async def detection(data: ModelOrderImageIn):
+    result = Atc.ClickIcon_Order(order_img_base64=data.order_img_base64,target_img_base64=data.target_img_base64)
+    return {"result": result }
+
+
+@app.post("/detection/text",summary="按序文字,返回坐标",tags=["目标检测"])
+async def detection(data: ModelImageIn):
+    result = Atc.Detection_Text(data.img_base64)
+    return {"result": result}
+
+@app.post("/detection/text/order",summary="按序返回文字的坐标",tags=["目标检测"])
+async def detection(data: ModelOrderImageIn):
+    result = Atc.ClickText_Order(order_img_base64=data.order_img_base64,target_img_base64=data.target_img_base64)
+    return {"result": result }
+
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="0.0.0.0", port=6688, access_log=True)
+
